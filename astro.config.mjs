6c282beeebf3b3d5ede518b387e81ab9dff6bc45
Anuj Paudel <anujpaudel98@yahoo.com>
2025-05-17 20:04:23 +0545

    Add sitemap integration and update favicon links; remove unused assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +11,8 @@ export default defineConfig({
     tailwind({
       // Configure theme customization
       config: { path: './tailwind.config.cjs' },
-    })
+    }),
+    sitemap()
   ],
   markdown: {
     shikiConfig: {
